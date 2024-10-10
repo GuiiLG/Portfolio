e01ac3dbc83546965d2dd12525d8cf7b13b213f2
luigi <guiluigiribeiro@gmail.com>
2024-10-10 09:53:58 -0300

    New changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Add custom JavaScript here
+// Close button modal
+function typingScript(){
+    const text = "I'm Luigi."
+    const typingText = document.querySelector('.typing-text')
+    const typingDelay = 100
+    typeText(text, typingText, typingDelay)
+}
+function typeText(text, typeText, typingDelay){
+    for(let i = 0; i < text.length; i++){
+        setTimeout(()=>{
+            typeText.textContent += text.charAt(i);
+        }, typingDelay * i)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', typingScript)
